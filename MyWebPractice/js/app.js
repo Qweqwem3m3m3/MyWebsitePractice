ad94a0c33ecfef65a1ab37bf481d5448cc39ed21
Qweqwem3m3m3 <97831286+Qweqwem3m3m3@users.noreply.github.com>
2022-04-09 01:07:50 +0800

    Add files via upload

---
+++ MyWebPractice/js/app.js
@@ -0,0 +1,35 @@
+function onImageLoaded(url, cb) {
+	var image = new Image()
+	image.src = url
+	if (image.complete) {
+		cb()
+	} else {
+		image.onload = function () {
+			cb()
+		}
+	}
+}
+
+$(function () {
+	var test = /_min\./
+	$("img").each(function (index, obj) {
+		if (test.test($(this).attr("src"))) {
+			var tmp = $(this)
+			var reSrc = $(this).attr("src").replace(test, ".");
+			onImageLoaded(reSrc, function () {
+				tmp.attr("src", reSrc)
+			})
+		}
+	})
+
+	$("body").each(function (index, obj) {
+		if (test.test($(this).css('background-image'))) {
+			var tmp = $(this)
+			var reSrc = $(this).css('background-image').replace(test, ".");
+			reSrc = reSrc.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+			onImageLoaded(reSrc, function () {
+				tmp.css('background-image', 'url(' + reSrc + ')');
+			})
+		}
+	})
+})
